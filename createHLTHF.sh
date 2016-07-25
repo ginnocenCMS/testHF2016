aecda3b90d3b3010a6061eb60be3878b233290cd
Jian Sun <sun229@purdue.edu>
2016-07-25 12:49:41 -0400

    add new L1 menu and just keep D0 to kpi channel on

--- createHLTHF.sh
+++ createHLTHF.sh
@@ -2,7 +2,7 @@
 fileStr='--input root://cms-xrd-global.cern.ch//store/user/gsfs/HIJINGpPb_MinBias816/HIJING_MinBias_pPb_RAWDEBUG_20160514/160514_124027/0000/step2_MinBias_pPb_DIGI_L1_DIGI2RAW_HLT_1.root'
 fileStrSec='--input root://cms-xrd-global.cern.ch//store/user/gsfs/HIJINGpPb_MinBias816/HIJING_MinBias_pPb_RECODEBUG_20160514/160514_215359/0000/step3_MinBias_pPb_RAW2DIGI_L1Reco_RECO_1.root'
 #hltGetConfiguration /dev/CMSSW_8_0_0/GRun --full --offline --mc --unprescale --process TEST --globaltag auto:run2_mc_GRun --l1-emulator 'Full' --output none --max-events 500 $fileStr> hlt_stage2_MC.py
-hltGetConfiguration /users/ginnocen/Dmeson2016pPb/DHFtrigger/V3 --full --offline --mc --unprescale --process TEST --globaltag auto:run2_mc_GRun --l1-emulator 'Full' --max-events 500 $fileStr> hlt_stage2_MC.py
+hltGetConfiguration /users/ginnocen/Dmeson2016pPb/DHFtrigger/V3 --full --offline --mc --unprescale --process TEST --globaltag auto:run2_mc_GRun --l1-emulator 'Full' --l1Xml L1Menu_Collisions2016_v3_ugt_1board_modified_pPb_Spectra.xml --max-events 500 $fileStr> hlt_stage2_MC.py
 hltConfigFromDB --cff --configName /dev/CMSSW_8_0_0/GRun --nopaths --services -PrescaleService,-EvFDaqDirector,-FastMonitoringService > setup_cff.py
 sed -i '/process = cms.Process( "TEST" )/a process.load("setup_cff")' hlt_stage2_MC.py
 
@@ -31,6 +31,6 @@ process.Dfinder.tktkRes_svpvDistanceCut_lowptD = cms.vdouble(0., 0., 0., 0., 0.,
 process.Dfinder.tktkRes_svpvDistanceCut_highptD = cms.vdouble(0., 0., 0., 0., 0., 0., 0., 0., 1.5, 1.5, 1.5, 1.5)
 process.Dfinder.svpvDistanceCut_lowptD = cms.vdouble(2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 0., 0., 0., 0.)
 process.Dfinder.svpvDistanceCut_highptD = cms.vdouble(2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 0., 0., 0., 0.)
-process.Dfinder.Dchannel = cms.vint32(1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1)
+process.Dfinder.Dchannel = cms.vint32(1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 process.p = cms.Path(process.DfinderSequence)
 '>> hlt_stage2_MC.py
